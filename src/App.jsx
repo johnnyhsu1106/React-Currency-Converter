3e3f8c82a581898ff17010ac4abb3f2bc4319a85
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-09-09 16:51:49 -0500

    add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import CurrencyConverter from './components/CurrencyConverter';
 import { CurrencyProvider } from './context/CurrencyContext';
-import './App.css'
+import './App.css';
+import './index.css';
 
 
 const App = () => {
