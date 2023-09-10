f14ceaf1232cc9364fbcecab34eb2c1709f416ff
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-09-09 22:39:51 -0500

    remove extra comma

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,5 @@ import { CurrencyProvider } from './context/CurrencyContext';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
