db1a70bb1f91e2a387f960872910d533fbc3ca5d
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-10-08 22:53:18 -0500

    move index.css to main.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import CurrencyConverter from './components/CurrencyConverter';
 import { CurrencyProvider } from './context/CurrencyContext';
 import './App.css';
-import './index.css';
-
 
 const App = () => {
   return (
